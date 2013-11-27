75bf087e459c1bef4e81bce33044b53c1758bcf0
Vadim Zaytsev <zaic101@gmail.com>
2013-11-27 22:52:03 +0700

    Add mass evaluations.

--- graph.rb
+++ graph.rb
@@ -43,6 +43,7 @@ class VariableNode < GraphNode
     end
 
     def generate_code
+        return if not @type
         @type.to_s + " " + code_name + " = " + value.to_s + ";"
     end
 
@@ -96,7 +97,12 @@ end
 
 class ReaspectGraph
 
-    attr_reader :constants, :variables, :functions, :order
+    attr_reader :constants, :variables, :arrays, :functions, :order
+
+    def eval_index(expression, counters = {})
+        # expression = (counters.merge(@constants)).reduce('') { |str, cur| str +=  "#{cur[0]} = #{cur[1]}; " } + expression
+        eval(expression)
+    end
 
     def fill_constants(statement)
         # ToDo check for redefinition
@@ -110,28 +116,87 @@ class ReaspectGraph
     def fill_variables(statements)
         # ToDo check for redefinition
         statements.select{ |st| st[:statement] == :variable }.each do |st|
-            st[:variable].each do |var_name| 
-                @variables[var_name] = VariableNode.new(var_name, st[:typename])
+            st[:variable].each do |var|
+                var_name = var[:name]
+                if var[:dims].size > 0 then
+                    dims = var[:dims].map(&:to_i) # ToDo: use eval
+                    @arrays[var_name] = dims
+                    dims.reduce(:*).times do |id|
+                        cid = id
+                        var_name_with_id = dims.reverse.map { |i| res, cid = cid % i, cid / i; res }.reverse.reduce(var_name) { |str, cur| str += '[' + cur.to_s + ']' }
+                        @variables[var_name_with_id] = VariableNode.new(var_name_with_id, nil)
+                    end
+                else
+                    @variables[var_name] = VariableNode.new(var_name, st[:typename])
+                end
             end
         end
     end
 
+    def add_scalar_function(st)
+        node = FunctionNode.new(st[:name])
+        st[:arguments].each do |arg|
+            arg_node = @variables[arg[:name]]
+            raise GraphException.new "Unknown variable '#{arg}' in function '#{st[:name]}' arguments." unless arg_node
+            node.in << arg_node
+            arg_node.out << node
+        end
+        st[:result].each do |res|
+            res_node = @variables[res[:name]]
+            raise GraphException.new "Unknown variable '#{res}' in function '#{st[:name]}' result." unless res_node
+            node.out << res_node
+            res_node.in << node
+        end
+        @functions[st[:name]] = node
+    end
+
+    def generate_index_values(dims, counters = {})
+        return [counters.clone] if dims.empty?
+
+        name = dims.first[:counter]
+        lower_bound = eval_index(dims.first[:bound_from], counters).to_i
+        upper_bound = eval_index(dims.first[:bound_to], counters).to_i
+        result = []
+        (lower_bound...upper_bound).each do |i|
+            counters[name] = i
+            result << generate_index_values(dims[1..-1], counters)
+        end
+        result.flatten
+    end
+
+    def generate_var_name(arg, counters)
+        p arg
+        name = arg[:name]
+        if @variables.has_key?(name) then
+            return name
+        elsif @arrays.has_key?(name) then
+            return name + arg[:dims].map{ |var| '[' + eval_index(var, counters).to_s + ']' }.join
+        else
+            return eval_index(name, counters)
+        end
+    end
+
+    def add_mass_function(st)
+        p "!!!!!!"
+        p st[:mass]
+        generate_index_values(st[:mass]).each do |counters|
+            p counters
+            cur_st = {}
+            st[:arguments].each { |arg| cur_st[:arguments] = generate_var_name(arg, counters) }
+            st[:result].each { |arg| cur_st[:result] = generate_var_name(arg, counters) }
+            p st
+            p cur_st
+        end
+        p "???????"
+    end
+
     def fill_functions(statements)
         statements.select{ |st| st[:statement] == :function }.each do |st|
-            node = FunctionNode.new(st[:name])
-            st[:arguments].each do |arg|
-                arg_node = @variables[arg]
-                raise GraphException.new "Unkonwn varialbe '#{arg}' in function '#{st[:name]}' arguments." unless arg_node
-                node.in << arg_node
-                arg_node.out << node
+            if st[:mass].size == 0
+                add_scalar_function(st)
+            else
+                add_mass_function(st)
             end
-            st[:result].each do |res|
-                res_node = @variables[res]
-                raise GraphException.new "Unkonwn varialbe '#{res}' in function '#{st[:name]}' result." unless res_node
-                node.out << res_node
-                res_node.in << node
-            end
-            @functions[st[:name]] = node
         end
     end
 
@@ -141,14 +206,14 @@ class ReaspectGraph
                 when :input then
                     st[:variables].each do |var| 
                         variable = @variables[var[:name]] 
-                        raise GraphException.new "Unkonwn input variable '#{var[:name]}'" unless variable
+                        raise GraphException.new "Unknown input variable '#{var[:name]}'" unless variable
                         @input << variable
                         variable.value = var[:value]
                     end
                 when :output then 
                     st[:variables].each do |var| 
-                        variable = @variables[var] 
-                        raise GraphException.new "Unkonwn output variable '#{var}'" unless variable
+                        variable = @variables[var[:name]]
+                        raise GraphException.new "Unknown output variable '#{var}'" unless variable
                         @output << variable
                     end
             end
@@ -162,7 +227,7 @@ class ReaspectGraph
     end
 
     def initialize(parser_result)
-        @variables = {}
+        @arrays, @variables = {}, {}
         @functions = {}
         @input, @output = [], []
         @constants = {}
